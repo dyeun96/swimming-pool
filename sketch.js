f1a31268f9467f8a0a69dad99bfbf51aaf834c31
dyeun96 <dyeun96@naver.com>
2014-12-04 15:04:18 +0900

    dfsf

--- sketch.js
+++ sketch.js
@@ -3,14 +3,20 @@ var bg;
 function setup() {
 
   bg = loadImage("bg.jpg");
-  img = loadImage("people.png");
+  people = loadImage("people.png");
+  people = loadImage("people_water.png")
 
   createCanvas(720, 480);
 }
 function draw() {
   background(bg);
+  var tDig = (new Date()%1000)/1000;
 
 //사람
-image(img, 451.340)
+if(tDig < 0.5){
+  image(people,451,340);
+}else{
+  image(people_water,451.340)
+}
 
   }
