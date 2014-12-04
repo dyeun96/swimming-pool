0793851f6034f184a43db15a20c822f75fa2389d
dyeun96 <dyeun96@naver.com>
2014-12-04 14:44:01 +0900

    c

--- sketch.js
+++ sketch.js
@@ -8,14 +8,3 @@ function setup() {
   bg = loadImage("bg.jpg");
   createCanvas(720, 480);
 }
-function draw() {
-  background(bg);
-  
-  stroke(226, 204, 0);
-  line(0, y, width, y);
-  
-  y++;
-  if (y > height) {
-    y = 0; 
-  }
-}
