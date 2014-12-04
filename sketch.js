9a31368dedd3322e630151f4da4dfb28c6a24460
dyeun96 <dyeun96@naver.com>
2014-12-04 15:32:07 +0900

    dfsfs

--- sketch.js
+++ sketch.js
@@ -10,16 +10,9 @@ function setup() {
 }
 function draw() {
   background(bg);
-  var tDig = (new Date()%6000)/6000;
 
 //사람
-image(people, 451, 340);
+image(people, 451, 343, 504.374);
 
-//다리 올린 사람
-if(tDig < 0.5){
-  image(people,451,340);
-}else{
-  image(people_water,451.340,504.374)
-}
 
   }
