0d15abd7edae02b1bd6714152bafd6c1637223b9
dyeun96 <dyeun96@naver.com>
2014-12-04 14:41:01 +0900

    a

---
+++ sketch.js
@@ -0,0 +1,10 @@
+var bg;
+var y = 0;
+
+function setup() {
+  // The background image must be the same size as the parameters
+  // into the size() method. In this program, the size of the image
+  // is 710x400 pixels.
+  bg = loadImage("bg.jpg");
+  createCanvas(720, 480);
+}
