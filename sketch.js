aa1d4e10572f7f930bd4b037a30e41f760967b0b
dyeun96 <dyeun96@naver.com>
2014-12-04 14:56:20 +0900

    da

--- sketch.js
+++ sketch.js
@@ -4,10 +4,13 @@ function setup() {
 
   bg = loadImage("bg.jpg");
   img = loadImage("people.png");
-  
+
   createCanvas(720, 480);
 }
 function draw() {
   background(bg);
+
+//사람
+image(img, 451.340)
 
   }
