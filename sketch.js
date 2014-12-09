a59b0cccd430bb061972feaf74bace34aa4e6583
dyeun96 <dyeun96@naver.com>
2014-12-09 17:24:05 +0900

    dfsf

--- sketch.js
+++ sketch.js
@@ -35,19 +35,5 @@ function draw() {
     if(mouseX>451 && mouseX <500 && mouseY >343 && mouseY <373){
       isLeg = !isLeg;
     }
-
-
-//사람2_둥둥떠다니는 사람
-
-  if(people2){
-    var time = (new Date()%2000)/2000;
-    if(time <0.5){
-      image(people, 451, 343);
-    }
-
-  function mousePressed(){
-    if(timing*3 <1){
-    image(people2, timing*899,568);  
-   }
      
   }
