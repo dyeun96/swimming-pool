c1940b8fdf4c9a84ee6159fa0c8324f6c5642a21
dyeun96 <dyeun96@naver.com>
2014-12-10 21:48:21 +0900

    fff

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var isLeg;
 var timing;
 var img;
 var song;
-
+var line1;
 
 function setup() {
 
@@ -12,6 +12,12 @@ function setup() {
   leg = loadImage("people_leg.png");
   people2 = loadImage("people2.png")
   isLeg =false;
+  line1 = loadImage("line1.png")
+  line2 = loadImage("line2.png")
+  line3 = loadImage("line3.png")
+  line4 = loadImage("line4.png")
+  line5 = loadImage("line5.png")
+  line6 = loadImage("line6.png")
 
   createCanvas(720, 480);
 }
@@ -19,17 +25,27 @@ function setup() {
 function draw() {
   background(bg);
   var t1 = (new Date()%18000)/18000;
-  var isLeg = new Date()%2000/2000;
+  var isLeg = new Date()%4000/4000;
+  var line1 = new Date()%000/4000;
      
 //물에 떠다니는 사람
 
     image(people2,300+sin(t1*2*PI)*80,160,85,93);
 
 //발 담그는 사람
+
     if(isLeg < 0.5){
       image(people,451,343);
     }else{
       image(leg,451,324);
     }
+
+//물결
+
+    if(line1 < 0.5){
+      image(line1,460,307)
+    }else{}
+
+
 
   }
