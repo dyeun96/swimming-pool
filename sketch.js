76be9d106e40ef6bd743fb725b8a284617358925
dyeun96 <dyeun96@naver.com>
2014-12-10 20:48:22 +0900

    fff

--- sketch.js
+++ sketch.js
@@ -22,9 +22,24 @@ function draw() {
      
 //물에 떠다니는 사람
 
-    image(people2,300+sin(t1*2*PI)*80,160,85,93);
+  image(people2,300+sin(t1*2*PI)*80,160,85,93);
 
 //발 담그는 사람
 
+  if(isLeg){
+    var time = (new Date()%2000)/2000;
+    if(time <0.5){
+      image(people, 451, 343);
+    }else{
+      image(leg, 451, 324);
+    }
+  }else{
+    image(people, 451, 343);
+  }
 
+}
+  function mousePressed(){
+    if(mouseX>451 && mouseX <500 && mouseY >343 && mouseY <373){
+      isLeg = !isLeg;
+    }
   }
