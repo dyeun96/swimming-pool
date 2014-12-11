b689d59b7b3c0e5c00a4ea6d9072366cbd4b23b4
dyeun96 <dyeun96@naver.com>
2014-12-11 14:32:33 +0900

    sss

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ function setup() {
 function draw() {
   background(bg);
   var t1 = (new Date()%18000)/18000;
-  var isLeg = new Date()%4000/4000;
+  var isLeg = new Date()%2000/2000;
   var waveTiming = (new Date()%1000)/1000;
 
      
